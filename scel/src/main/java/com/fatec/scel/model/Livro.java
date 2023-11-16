f879ddc1a9d61b78085048a89fe023b552045d2e
ffvprogweb <disclabes@gmail.com>
2023-11-16 15:06:09 -0300

    versao inicial

---
+++ scel/src/main/java/com/fatec/scel/model/Livro.java
@@ -0,0 +1,89 @@
+package com.fatec.scel.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Years;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Livro {
+	String isbn;
+	String tiluo;
+	String autor;
+	int edicao;
+	String ano;
+	Logger logger = LogManager.getLogger(this.getClass());
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTiluo() {
+		return tiluo;
+	}
+
+	public void setTiluo(String tiluo) {
+		this.tiluo = tiluo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getEdicao() {
+		return edicao;
+	}
+
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public boolean ano_da_publicacao_maior_que_o_ano_atual(String anoPublicacao) {
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY");
+		DateTime dt1 = fmt.parseDateTime(anoPublicacao);
+		if (dt1.isBeforeNow() || dt1.isEqualNow()) {
+			logger.info(">>>>>> ano da publicacao valido");
+			return true;
+		}
+		return false;
+	}
+
+	public boolean verificaAnoPublicacao(String anoPublicacao) {
+		try {
+			String dataPublicacao = "01/01/" + anoPublicacao;
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
+			DateTime dtPublicacao = formatter.parseDateTime(dataPublicacao);
+			DateTime dtAtual = new DateTime();
+			System.out.println("anos => " +   Years.yearsBetween(dtAtual, dtPublicacao).getYears());
+			Days d = Days.daysBetween(dtAtual, dtPublicacao);
+			if (d.getDays() >= 0) {
+				logger.info(">>>>>> dataVencMaiorDataAtual => true ");
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			logger.info(">>>>>> metodo dtVencMaiorDtAtual() retornou erro nao esperado =>" + e.getMessage());
+			throw new IllegalArgumentException("Data invalida");
+		}
+	}
+
+}
---
+++ scel/src/test/java/com/fatec/scel/Req01CadastrarLivroTest.java
@@ -0,0 +1,28 @@
+package com.fatec.scel;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.fatec.scel.model.Livro;
+
+class Req01CadastrarLivroTest {
+
+	@Test
+	void ct01_quando_o_ano_publicacao_e_menor_que_o_ano_atual_deve_aceitar() {
+		Livro livro = new Livro();
+		assertTrue(livro.ano_da_publicacao_maior_que_o_ano_atual("2022"));
+	}
+
+	@Test
+	void ct02_quando_o_ano_publicacao_e_maior_que_o_ano_atual_deve_rejeitar() {
+		Livro livro = new Livro();
+		assertFalse(livro.ano_da_publicacao_maior_que_o_ano_atual("2024"));
+	}
+	@Test
+	void test3() {
+		Livro livro = new Livro();
+		assertTrue(livro.ano_da_publicacao_maior_que_o_ano_atual("2023"));
+	}
+}
